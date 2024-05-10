a4b2f57a382cd91fc53fdf7817d9ffc17b832668
eddiebus <edwinbusulwa@gmail.com>
2024-05-10 22:18:13 +0100

    Menu Animation and Title Screen
    Changes:
    
    > Updates to music player. Volume control fix
    > UI fixes on title menu (prefab pain) Less unwanted prefab overrides
    > UI Navigation fixed - Key nav is back

--- Assets/Script/Sounds/MusicSpeaker.cs
+++ Assets/Script/Sounds/MusicSpeaker.cs
@@ -6,7 +6,9 @@ public class MusicSpeaker : MonoBehaviour
     public float Volume = 1.0f;
     public bool Loop;
     public bool PlayOnStart = true;
-    protected Sound MusicSound = null; 
+    protected Sound MusicSound = null;
+
+    public UnityEngine.Events.UnityEvent OnSongEnd;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,8 +38,14 @@ public class MusicSpeaker : MonoBehaviour
 
             MusicSound.OnSoundDestroy += () => {
                 MusicSound = null;
+                OnSongEnd.Invoke();
             };   
         }
+    }
+
+    public void SetVolume(float newVolume)
+    {
+        Volume = newVolume;
     }
 
     // Update Volume on existing Audioclip(s)
